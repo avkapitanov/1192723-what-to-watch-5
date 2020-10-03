85079b06122d20fc5070cad62f6482f449b75602
Artem Kapitanov <avkapitanov@yandex.ru>
2020-10-03 14:05:02 +0300

    Add routing to project

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import MainPage from "../main-page/main-page";
+import LoginPage from "../login-page/login-page";
+import FilmsPage from "../films-page/films-page";
+import FilmPage from "../film-page/film-page";
+import PlayerPage from "../player-page/player-page";
 import PromoFilm from "../promo-film/promo-film";
-
+import FilmReviewsPage from "../film-reviews-page/film-reviews-page";
 
 const App = (props) => {
   const {promoFilm} = props;
 
   return (
-    <React.Fragment>
-      <PromoFilm title={promoFilm.title} genre={promoFilm.genre} year={promoFilm.year}/>
-      <MainPage/>
-    </React.Fragment>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <React.Fragment>
+            <PromoFilm title={promoFilm.title} genre={promoFilm.genre} year={promoFilm.year}/>
+            <MainPage/>
+          </React.Fragment>
+        </Route>
+        <Route exact path="/login">
+          <LoginPage/>
+        </Route>
+        <Route exact path="/mylist">
+          <FilmsPage/>
+        </Route>
+        <Route exact path="/films/:id/review">
+          <FilmReviewsPage/>
+        </Route>
+        <Route exact path="/films/:id">
+          <FilmPage/>
+        </Route>
+        <Route exact path="/player/:id">
+          <PlayerPage/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
