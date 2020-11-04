7e5e9fd927267eb71a4bd3c145228c32a6d4994e
Artem Kapitanov <avkapitanov@yandex.ru>
2020-11-04 21:46:08 +0300

    Add several snapshot tests

---
+++ src/components/films-filter/films-filter.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {FilmsFilter} from "./films-filter";
+
+const filterGenres = [
+  `All genres`,
+  `Drama`,
+  `Comedy`,
+  `Horror`,
+  `Romance`,
+  `Sci-Fi`
+];
+
+const selectedFilterGenre = `Drama`;
+
+it(`FilmsFilter component render correctly`, () => {
+  const tree = renderer.create(
+      <FilmsFilter
+        filterGenres={filterGenres}
+        selectedFilterGenre={selectedFilterGenre}
+        changeFilterGenreAction={() => {}}
+      />
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
